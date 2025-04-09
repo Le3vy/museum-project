c7e157d472210aec5899cb79f824a75d6de8a9b5
Posa Levente <h376512@stud.u-szeged.hu>
2025-04-09 19:13:56 +0200

    asd

---
+++ src/app/pages/exhibition/exhibition.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exhibition',
+  imports: [],
+  templateUrl: './exhibition.component.html',
+  styleUrl: './exhibition.component.scss'
+})
+export class ExhibitionComponent {
+
+}
