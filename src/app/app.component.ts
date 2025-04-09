d35c3129a089cbfbad189f6583e537be05026467
Posa Levente <h376512@stud.u-szeged.hu>
2025-04-09 21:38:10 +0200

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './shared/menu/menu.component';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterOutlet,
+    MenuComponent,
+    RouterLink
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'museum-project';
+
+  onToggleSidenav(sidenav: MatSidenav){
+    sidenav.toggle();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ExhibitionComponent } from './pages/exhibition/exhibition.component';
+import { TicketPurchaseComponent } from './pages/ticket-purchase/ticket-purchase.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home',component: HomeComponent},
+    {path: 'profile',component: ProfileComponent},
+    {path: 'exhibition',component: ExhibitionComponent},
+    {path: 'ticket-purchase',component: TicketPurchaseComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: '**', component:HomeComponent}
+];
