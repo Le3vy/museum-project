2a41fd18c9bceb21465b8b37beafca73aad7b324
Posa Levente <h376512@stud.u-szeged.hu>
2025-04-11 15:26:48 +0200

    home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  exhibitions = [
+    { name: 'Ancient Egypt', image: 'images/aegypt.png' }, 
+    { name: 'Medieval Europe', image: 'images/meurope.png' },
+    { name: 'Industrial Revolution', image: 'images/irevolution.png' },
+    { name: 'World War History', image: 'images/ww2.png' }
+  ];
 }
